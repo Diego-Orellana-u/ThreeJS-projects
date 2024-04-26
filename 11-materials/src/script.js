673ee78d322e10971412df56dfa93135730a848e
Diego Orellana <diego.orellana.git@gmail.com>
2024-04-26 14:22:33 -0400

    started materials exercise

--- 11-materials/src/script.js
+++ 11-materials/src/script.js
@@ -1,78 +1,128 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+// Textures
+const textureLoader = new THREE.TextureLoader();
+
+const doorColorTexture = textureLoader.load('textures/door/color.jpg');
+const doorAlphaTexture = textureLoader.load('textures/door/alpha.jpg');
+const doorAmbientOcclussionTexture = textureLoader.load(
+  'textures/door/ambientOcclusion.jpg'
+);
+const doorHeightTexture = textureLoader.load('textures/door/height.jpg');
+const doorNormalTexture = textureLoader.load('textures/door/normal.jpg');
+const doorMetalnessTexture = textureLoader.load('textures/door/metalness.jpg');
+const doorRoughnessTexture = textureLoader.load('textures/door/roughness.jpg');
+const matcapTexture = textureLoader.load('/textures/matcaps/1.png');
+const gradientTexture = textureLoader.load('/textures/gradients/3.jpg');
 
 /**
  * Base
  */
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector('canvas.webgl');
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 /**
- * Sizes
+ * Sizesx
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+// Mesh
+const material = new THREE.MeshBasicMaterial();
+material.map = doorColorTexture;
+
+const sphereMesh = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 16, 16),
+  material
+);
+const planeMesh = new THREE.Mesh(new THREE.PlaneGeometry(1, 1), material);
+const torusMesh = new THREE.Mesh(
+  new THREE.TorusGeometry(0.3, 0.2, 16, 32),
+  material
+);
+
+scene.add(sphereMesh, planeMesh, torusMesh);
+
+sphereMesh.position.x = -1.5;
+torusMesh.position.x = 1.5;
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 1
-camera.position.y = 1
-camera.position.z = 2
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.x = 1;
+camera.position.y = 1;
+camera.position.z = 4;
+
+camera.position.x = 5;
+camera.lookAt(new THREE.Vector3(10, 3, 10));
+
+scene.add(camera);
 
 // Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 /**
  * Animate
  */
-const clock = new THREE.Clock()
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+const clock = new THREE.Clock();
 
-    // Update controls
-    controls.update()
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
 
-    // Render
-    renderer.render(scene, camera)
+  sphereMesh.rotation.y = 0.1 * elapsedTime;
+  planeMesh.rotation.y = 0.1 * elapsedTime;
+  torusMesh.rotation.y = 0.1 * elapsedTime;
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
+  sphereMesh.rotation.x = 0.15 * elapsedTime;
+  planeMesh.rotation.x = 0.15 * elapsedTime;
+  torusMesh.rotation.x = 0.15 * elapsedTime;
 
-tick()
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
